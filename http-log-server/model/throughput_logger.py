952eb9b3c897723b832c8472c317df79ffdc96a3
Lucas <lucas@prevision.com.br>
2022-05-17 20:18:22 -0300

    Add log

--- http-log-server/model/throughput_logger.py
+++ http-log-server/model/throughput_logger.py
@@ -1,5 +1,8 @@
 import os
 import csv
+from time import time_ns
+
+from sqlalchemy import BigInteger
 from model.helpers import DirGetter
 
 class ThroughputLogger(object):
@@ -16,7 +19,6 @@ class ThroughputLogger(object):
         self.csv_file = self.dir_getter.source_thoughput_log()
         self.file_directory = os.path.split(self.csv_file)[0]
         self.file_name = os.path.split(self.csv_file)[1]
-        self.csv_columns = ["throughtput_counter"]
 
         if not os.path.isdir(self.file_directory): os.makedirs(self.file_directory)
         if not os.path.isfile(self.csv_file): self._append_data()
@@ -31,26 +33,31 @@ class ThroughputLogger(object):
 
     def _current_counter(self):
         csvfile = open(self.csv_file, 'r')
-        reader = csv.DictReader(csvfile)
+        reader = csv.reader(csvfile, delimiter=';')
         all_lines = list(reader)
-        last_line = all_lines[-1]
+        if len(all_lines) > 0:
+            last_line = all_lines[-1]
+        else:
+            last_line = 0
         return last_line
 
     def _append_data(self):
         try:
-            csvfile = open(self.csv_file, 'r')
-            reader = csv.DictReader(csvfile, self.csv_columns)
-            all_lines = list(reader)
-            last_line = 0
-            try:
-                last_line = all_lines[-1]
-            except:
-                last_line = 0
+            last_line = self._current_counter()
+            current_time = time_ns() - int(last_line[0])
+            counter = int(last_line[1]) + 1
 
             with open(self.csv_file, 'a+', newline='') as csvfile:
                 spamwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow([int(last_line['throughtput_counter']) + 1])
+                spamwriter.writerow(
+                    [
+                        current_time,
+                        counter
+                    ]
+                )
                 csvfile.close()
         except IOError:
             return False
+
+
         return True
