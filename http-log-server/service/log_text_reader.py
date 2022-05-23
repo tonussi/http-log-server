dd01fb95546db89dad1ed7b04e7f05ff2dd5c48b
Lucas <lucas@prevision.com.br>
2022-05-23 02:25:19 -0300

    Add

--- http-log-server/service/log_text_reader.py
+++ http-log-server/service/log_text_reader.py
@@ -17,16 +17,22 @@ class LogTextReader(object):
         result_text_line = ""
         line_number_parsed = int(line_number)
         if line_number_parsed == 0:
-            line_number_parsed = 1
+            line_number_parsed = -1
 
         try:
             with open(self.dir_getter.source_db_file_path(), 'r') as log_main_file:
-                result_text_line = self._search_line_by_line_in_file(line_number_parsed, log_main_file)
+                result_text_line = self.decide_what_search_line_to_use(line_number_parsed, log_main_file)
                 log_main_file.close()
         except IOError:
             return "log is not present"
 
         return result_text_line
+
+    def decide_what_search_line_to_use(self, line_number_parsed, log_main_file):
+        if line_number_parsed is -1:
+            return self._last_line_instead(log_main_file)
+
+        return self._search_line_by_line_in_file(line_number_parsed, log_main_file)
 
     def _search_line_by_line_in_file(self, line_number_parsed, log_main_file):
         counter = 1
@@ -38,3 +44,8 @@ class LogTextReader(object):
                 return stripped_line
 
             counter += 1
+
+    def _last_line_instead(self, log_main_file):
+        for line in log_main_file:
+            stripped_line = line.strip()
+        return stripped_line
