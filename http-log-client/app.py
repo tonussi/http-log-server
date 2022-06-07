241c76d0b61914ea989d2c81f8dd7de656b418b6
Lucas <lucas@prevision.com.br>
2022-06-07 04:10:55 -0300

    Fx

--- http-log-client/app.py
+++ http-log-client/app.py
@@ -17,12 +17,12 @@ load_dotenv()
 @click.option("--address", default="localhost", help="Server address")
 @click.option("--port", default=8000, help="Server port")
 @click.option("--payload_size", default=1, help="Set the payload size")
-@click.option("--qty_iteration", default=10000, help="Set the key range to determine the volume")
-@click.option("--read_rate", default=5, help="Set the reading rate from 0 to 100 percent")
-@click.option("--n_threads", default=15, help="Set number of client threads")
+@click.option("--qty_iteration", default=1000000, help="Set the key range to determine the volume")
+@click.option("--read_rate", default=50, help="Set the reading rate from 0 to 100 percent")
+@click.option("--n_threads", default=30, help="Set number of client threads")
 @click.option("--thinking_time", default=0.2, help="Set thinking time between requests default is 200ms")
-@click.option("--percentage_sampling", default=90, help="Percentage of log in total")
-@click.option("--duration", default=0.5, help="Duration in seconds")
+@click.option("--percentage_sampling", default=95, help="Percentage of log in total")
+@click.option("--duration", default=1.5, help="Duration in seconds")
 def hello(**kwargs):
     threads = []
     num_threads = kwargs["n_threads"]
