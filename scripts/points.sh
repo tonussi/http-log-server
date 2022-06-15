594945daf179f6a0a7c09eb593ddb7a3f1858f86
Lucas <lucas@prevision.com.br>
2022-06-15 20:10:36 -0300

    Add

--- scripts/points.sh
+++ scripts/points.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env sh
 
-sh ./scripts/experiment.sh kubernetes 2 1 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 4 1 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 4 2 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 8 1 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 8 2 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 16 1 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 16 2 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 32 1 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 32 2 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 64 1 50 e1/128-rw
-sh ./scripts/experiment.sh kubernetes 24 3 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 36 2 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 38 2 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 42 2 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 32 3 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 48 2 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 52 2 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 36 3 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 42 3 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 64 2 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 128 1 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 44 3 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 64 3 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 52 4 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 64 4 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 128 2 50 e1/128-rw
-# sh ./scripts/experiment.sh kubernetes 72 4 50 e1/128-rw
+sh ./scripts/experiment.sh kubernetes 1 $(expr 2 \/ 1) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 1 $(expr 4 \/ 1) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 4 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 1 $(expr 8 \/ 1) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 8 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 1 $(expr 16 \/ 1) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 16 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 1 $(expr 32 \/ 1) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 32 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 1 $(expr 64 \/ 1) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 3 $(expr 24 \/ 3) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 36 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 38 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 42 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 3 $(expr 32 \/ 3) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 48 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 52 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 3 $(expr 36 \/ 3) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 3 $(expr 42 \/ 3) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 64 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 1 $(expr 128 \/ 1) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 3 $(expr 44 \/ 3) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 3 $(expr 64 \/ 3) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 4 $(expr 52 \/ 4) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 4 $(expr 64 \/ 4) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 2 $(expr 128 \/ 2) 50 raw/e1/128-2-rw
+sh ./scripts/experiment.sh kubernetes 4 $(expr 72 \/ 4) 50 raw/e1/128-2-rw
 
 sleep 1
 exit 0
