cf0b3fb8e7e0c2864ab27ffbbb38444351b2864b
Lucas <lucas@prevision.com.br>
2022-06-16 15:55:37 -0300

    Add

--- scripts/points.sh
+++ scripts/points.sh
@@ -1,58 +1,58 @@
 #!/usr/bin/env sh
 
-sh ./scripts/experiment.sh kubernetes 1 2 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 1 2 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 1 4 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 1 4 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 2 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 2 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 1 8 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 1 8 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 4 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 4 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 1 16 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 1 16 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 8 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 8 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 1 32 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 1 32 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 16 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 16 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 1 64 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 1 64 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 3 8 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 3 8 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 18 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 18 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 19 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 19 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 21 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 21 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 3 10 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 3 10 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 24 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 24 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 26 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 26 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 3 12 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 3 12 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 3 14 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 3 14 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 32 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 32 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 1 128 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 1 128 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 3 14 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 3 14 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 3 21 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 3 21 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 4 13 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 4 13 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 4 16 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 4 16 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 2 64 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 2 64 100 raw/e1/128-r
 sleep 10
-sh ./scripts/experiment.sh kubernetes 4 18 0 raw/e1/128-w
+sh ./scripts/experiment.sh kubernetes 4 18 100 raw/e1/128-r
 sleep 10
 
 sleep 1
